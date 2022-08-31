dd1028006ce65f11b3d8e8c97a1c7d52897ba6ec
5h4k4r <shakar.halabjai@yahoo.com>
2022-08-31 15:45:12 +0300

    Configure environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import { AppConfig } from 'src/app/interfaces/app-config';
+
+export const environment: AppConfig = {
+  production: true,
+  apiUrl: window["env"]["apiUrl"] || "default"
 };
