6986e5dde78f76818d43686823b80e1e278b9f58
5h4k4r <shakar.halabjai@yahoo.com>
2022-09-20 23:03:45 +0300

    Import pulled swagger module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ApiModule } from './backend/api.module';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     HttpClientModule,
+    ApiModule.forRoot({ rootUrl: '/api' })
   ],
   providers: [],
   bootstrap: [AppComponent]
